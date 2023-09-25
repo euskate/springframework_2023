9951b77dc2ce8242fa257a5269f5855ff4debd31
kktlove <kkt09072@gmail.com>
2023-09-25 14:10:16 +0900

    springframework setting of intelliJ

---
+++ study2/src/main/java/kr/ed/heabeop/domain/Test.java
@@ -0,0 +1,13 @@
+package kr.ed.heabeop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+    private int num;
+    private String title;
+}
